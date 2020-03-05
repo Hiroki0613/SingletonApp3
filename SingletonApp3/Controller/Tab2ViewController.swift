fcc81ee936fb9a8885887646dd79b3a670ea457e
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-06 00:14:43 +0900

    シングルトンの継承に不備あり？

--- SingletonApp3/Controller/Tab2ViewController.swift
+++ SingletonApp3/Controller/Tab2ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class Tab2ViewController: UITabBarController {
 
+    let singleton:Singleton = Singleton.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        var getSingletonTimes2 = singleton.getContents()
+        
+//        for get in getSingletonTimes2 {
+//            get = get * 2
+//        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            print("hirohiro")
+            print(getSingletonTimes2)
+               }
+        
+        
      
     }
     
