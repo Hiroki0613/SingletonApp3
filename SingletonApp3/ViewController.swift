a69ef28683ccdba8f46fd29fd01dcdacc5d5ca7f
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-05 23:46:40 +0900

    Initial Commit

---
+++ SingletonApp3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingletonApp3
+//
+//  Created by 近藤宏輝 on 2020/03/05.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
